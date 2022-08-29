47398af0ff01b5b8782292933426e48e91aa041a
chae-hwa <ihj0519@naver.com>
2022-08-29 18:03:08 +0900

    TestInitData 도입

--- src/test/java/com/example/query_dsl/UserRepositoryTests.java
+++ src/test/java/com/example/query_dsl/UserRepositoryTests.java
@@ -6,34 +6,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test") // 테스트 모드 활성화
 class UserRepositoryTests {
-
     @Autowired
     private UserRepository userRepository;
 
     @Test
     @DisplayName("회원 생성")
     void t1() {
-        SiteUser u1 = SiteUser.builder()
-                .username("user1")
+        SiteUser u3 = SiteUser.builder()
+                .username("user3")
                 .password("{noop}1234")
-                .email("user1@test.com")
+                .email("user3@test.com")
                 .build();
 
-        SiteUser u2 = SiteUser.builder()
-                .username("user2")
+        SiteUser u4 = SiteUser.builder()
+                .username("user4")
                 .password("{noop}1234")
-                .email("user2@test.com")
+                .email("user4@test.com")
                 .build();
 
-
-        userRepository.saveAll(Arrays.asList(u1, u2));
+        userRepository.saveAll(Arrays.asList(u3, u4));
     }
 
     @Test
@@ -43,8 +43,8 @@ class UserRepositoryTests {
 
         assertThat(u1.getId()).isEqualTo(1L);
         assertThat(u1.getUsername()).isEqualTo("user1");
-        assertThat(u1.getPassword()).isEqualTo("{noop}1234");
         assertThat(u1.getEmail()).isEqualTo("user1@test.com");
+        assertThat(u1.getPassword()).isEqualTo("{noop}1234");
     }
 
     @Test
@@ -54,7 +54,7 @@ class UserRepositoryTests {
 
         assertThat(u2.getId()).isEqualTo(2L);
         assertThat(u2.getUsername()).isEqualTo("user2");
-        assertThat(u2.getPassword()).isEqualTo("{noop}1234");
         assertThat(u2.getEmail()).isEqualTo("user2@test.com");
+        assertThat(u2.getPassword()).isEqualTo("{noop}1234");
     }
 }
