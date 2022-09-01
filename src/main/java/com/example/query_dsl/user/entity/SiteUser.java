7a83b411a711dd72438886c156357b41f779fcbb
chae-hwa <ihj0519@naver.com>
2022-09-01 17:09:04 +0900

    u2는 follower와 following을 알 수 있어야 한다.

--- src/main/java/com/example/query_dsl/user/entity/SiteUser.java
+++ src/main/java/com/example/query_dsl/user/entity/SiteUser.java
@@ -34,6 +34,10 @@ public class SiteUser {
     @ManyToMany(cascade = CascadeType.ALL)
     private Set<SiteUser> followers = new HashSet<>();
 
+    @Builder.Default
+    @ManyToMany(cascade = CascadeType.ALL)
+    private Set<SiteUser> followings = new HashSet<>();
+
     public void addInterestKeywordContent(String keywordContent) {
         interestKeywords.add(new InterestKeyword(keywordContent));
     }
@@ -44,5 +48,8 @@ public class SiteUser {
         if(this.getId() == following.getId()) return;
 
         following.getFollowers().add(this);
+
+        getFollowings().add(following);
     }
+
 }
